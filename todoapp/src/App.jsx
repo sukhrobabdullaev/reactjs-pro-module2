27a98fef80e42328bfc69943dcba7abde334c758
sukhrobabdullaev <sukhrobjonabdullaev3365@gmail.com>
2024-01-09 20:46:34 +0500

    added todo app initials

---
+++ todoapp/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Actions from "./components/Actions";
+import Header from "./components/Header";
+import Main from "./components/Main";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Actions />
+    </>
+  );
+};
+
+export default App;
---
+++ todoapp/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="text-light bg-primary border-bottom border-danger border-5">ToDo App</h1>
+      <p>
+        Ushbu saytimizdan foydalanib, kuningizni samarali vazifalar bilan
+        o'tkazing!
+      </p>
+    </div>
+  );
+};
+
+export default Header;
